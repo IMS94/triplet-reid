a82cf16612c3c507480b1e8ec0214d7519be6623
Imesha Sudasingha <imesha.sudasingha@gmail.com>
2018-05-03 14:37:25 +0530

    Included a class with adjustable parameters for convenience

--- lib/trinet.py
+++ lib/trinet.py
@@ -10,129 +10,154 @@ from aggregators import AGGREGATORS
 from lib.utils import compare
 
 
-def flip_augment(image, pid):
-    """ Returns both the original and the horizontal flip of an image. """
-    images = tf.stack([image, tf.reverse(image, [1])])
-    return images, [pid] * 2
+class TriNetReID:
+    def __init__(self, model="resnet_v1_50", head="fc1024", checkpoints="checkpoints", aggregator="mean",
+                 batch_size=16, crop_augment=None, flip=None, embed_dim=128):
+        self.crop_augment = crop_augment
+        self.flip = flip
+        self.embed_dim = embed_dim
+        self.model = model
+        self.head = head
+        self.aggregator = aggregator
+        self.checkpoints = checkpoints
+        self.batch_size = batch_size
 
+    @staticmethod
+    def flip_augment(image, pid):
+        """ Returns both the original and the horizontal flip of an image. """
+        images = tf.stack([image, tf.reverse(image, [1])])
+        return images, [pid] * 2
 
-def five_crops(image, crop_size):
-    """ Returns the central and four corner crops of `crop_size` from `image`. """
-    image_size = tf.shape(image)[:2]
-    crop_margin = tf.subtract(image_size, crop_size)
-    assert_size = tf.assert_non_negative(
-        crop_margin, message='Crop size must be smaller or equal to the image size.')
-    with tf.control_dependencies([assert_size]):
-        top_left = tf.floor_div(crop_margin, 2)
-        bottom_right = tf.add(top_left, crop_size)
-    center = image[top_left[0]:bottom_right[0], top_left[1]:bottom_right[1]]
-    top_left = image[:-crop_margin[0], :-crop_margin[1]]
-    top_right = image[:-crop_margin[0], crop_margin[1]:]
-    bottom_left = image[crop_margin[0]:, :-crop_margin[1]]
-    bottom_right = image[crop_margin[0]:, crop_margin[1]:]
-    return center, top_left, top_right, bottom_left, bottom_right
+    @staticmethod
+    def five_crops(image, crop_size):
+        """ Returns the central and four corner crops of `crop_size` from `image`. """
+        image_size = tf.shape(image)[:2]
+        crop_margin = tf.subtract(image_size, crop_size)
+        assert_size = tf.assert_non_negative(crop_margin,
+                                             message='Crop size must be smaller or equal to the image size.')
+        with tf.control_dependencies([assert_size]):
+            top_left = tf.floor_div(crop_margin, 2)
+            bottom_right = tf.add(top_left, crop_size)
+        center = image[top_left[0]:bottom_right[0], top_left[1]:bottom_right[1]]
+        top_left = image[:-crop_margin[0], :-crop_margin[1]]
+        top_right = image[:-crop_margin[0], crop_margin[1]:]
+        bottom_left = image[crop_margin[0]:, :-crop_margin[1]]
+        bottom_right = image[crop_margin[0]:, crop_margin[1]:]
+        return center, top_left, top_right, bottom_left, bottom_right
 
+    @staticmethod
+    def resize(image, image_size):
+        img_str = cv2.imencode('.jpg', image)[1].tostring()
+        image_decoded = tf.image.decode_jpeg(img_str, channels=3)
+        image_resized = tf.image.resize_images(image_decoded, image_size)
+        return image_resized
 
-def resize(image, image_size):
-    img_str = cv2.imencode('.jpg', image)[1].tostring()
-    image_decoded = tf.image.decode_jpeg(img_str, channels=3)
-    image_resized = tf.image.resize_images(image_decoded, image_size)
-    return image_resized
+    def embed(self, crops):
+        # Load the data from the CSV file.
+        net_input_size = (256, 128)
+        pre_crop_size = (288, 144)
 
+        crops = [self.resize(img, pre_crop_size if self.crop_augment else net_input_size) for img in crops]
+        dataset = tf.data.Dataset.from_tensor_slices((crops, [x for x in range(len(crops))]))
 
-def main(crops, crop_augment=None, flip=None):
-    # Load the data from the CSV file.
-    net_input_size = (256, 128)
-    pre_crop_size = (288, 144)
+        # Augment the data if specified by the arguments.
+        # `modifiers` is a list of strings that keeps track of which augmentations
+        # have been applied, so that a human can understand it later on.
+        modifiers = ['original']
+        if self.flip:
+            dataset = dataset.map(lambda img, index: self.flip_augment(img, index))
+            dataset = dataset.apply(tf.contrib.data.unbatch())
+            modifiers = [o + m for m in ['', '_flip'] for o in modifiers]
 
-    crops = [resize(img, pre_crop_size if crop_augment else net_input_size) for img in crops]
-    dataset = tf.data.Dataset.from_tensor_slices((crops, [x for x in range(len(crops))]))
+        if self.crop_augment == 'center':
+            dataset = dataset.map(lambda im, index: (self.five_crops(im, net_input_size)[0], index))
+            modifiers = [o + '_center' for o in modifiers]
+        elif self.crop_augment == 'five':
+            dataset = dataset.map(lambda im, index: (tf.stack(self.five_crops(im, net_input_size)), [index] * 5))
+            dataset = dataset.apply(tf.contrib.data.unbatch())
+            modifiers = [o + m for o in modifiers for m in
+                         ['_center', '_top_left', '_top_right', '_bottom_left', '_bottom_right']]
+        elif self.crop_augment == 'avgpool':
+            modifiers = [o + '_avgpool' for o in modifiers]
+        else:
+            modifiers = [o + '_resize' for o in modifiers]
 
-    # Augment the data if specified by the arguments.
-    # `modifiers` is a list of strings that keeps track of which augmentations
-    # have been applied, so that a human can understand it later on.
-    modifiers = ['original']
-    if flip:
-        dataset = dataset.map(lambda img, index: flip_augment(img, index))
-        dataset = dataset.apply(tf.contrib.data.unbatch())
-        modifiers = [o + m for m in ['', '_flip'] for o in modifiers]
+        # Group it back into PK batches.
+        dataset = dataset.batch(self.batch_size)
 
-    if crop_augment == 'center':
-        dataset = dataset.map(lambda im, index: (five_crops(im, net_input_size)[0], index))
-        modifiers = [o + '_center' for o in modifiers]
-    elif crop_augment == 'five':
-        dataset = dataset.map(lambda im, index: (tf.stack(five_crops(im, net_input_size)), [index] * 5))
-        dataset = dataset.apply(tf.contrib.data.unbatch())
-        modifiers = [o + m for o in modifiers for m in
-                     ['_center', '_top_left', '_top_right', '_bottom_left', '_bottom_right']]
-    elif crop_augment == 'avgpool':
-        modifiers = [o + '_avgpool' for o in modifiers]
-    else:
-        modifiers = [o + '_resize' for o in modifiers]
+        # Overlap producing and consuming.
+        dataset = dataset.prefetch(1)
 
-    # Group it back into PK batches.
-    dataset = dataset.batch(10)
+        images, _ = dataset.make_one_shot_iterator().get_next()
 
-    # Overlap producing and consuming.
-    dataset = dataset.prefetch(1)
+        # Create the model and an embedding head.
+        model = import_module('nets.' + self.model)
+        head = import_module('heads.' + self.head)
 
-    images, _ = dataset.make_one_shot_iterator().get_next()
+        endpoints, body_prefix = model.endpoints(images, is_training=False)
+        with tf.name_scope('head'):
+            endpoints = head.head(endpoints, self.embed_dim, is_training=False)
 
-    # Create the model and an embedding head.
-    model = import_module('nets.' + "resnet_v1_50")
-    head = import_module('heads.' + "fc1024")
+        with tf.Session() as sess:
+            # Initialize the network/load the checkpoint.
+            checkpoint = tf.train.latest_checkpoint(self.checkpoints)
+            print('Restoring from checkpoint: {}'.format(checkpoint))
+            tf.train.Saver().restore(sess, checkpoint)
 
-    endpoints, body_prefix = model.endpoints(images, is_training=False)
-    with tf.name_scope('head'):
-        endpoints = head.head(endpoints, 128, is_training=False)
+            # Go ahead and embed the whole dataset, with all augmented versions too.
+            emb_storage = np.zeros((len(crops) * len(modifiers), self.embed_dim), np.float32)
+            for start_idx in count(step=self.batch_size):
+                try:
+                    emb = sess.run(endpoints['emb'])
+                    print('\rEmbedded batch {}-{}/{}'.format(start_idx, start_idx + len(emb), len(emb_storage)),
+                          flush=True, end='')
+                    emb_storage[start_idx:start_idx + len(emb)] = emb
+                except tf.errors.OutOfRangeError:
+                    break  # This just indicates the end of the dataset.
 
-    with tf.Session() as sess:
-        # Initialize the network/load the checkpoint.
-        checkpoint = tf.train.latest_checkpoint("checkpoints")
-        print('Restoring from checkpoint: {}'.format(checkpoint))
-        tf.train.Saver().restore(sess, checkpoint)
+            print("Done with embedding, aggregating augmentations...", flush=True)
 
-        # Go ahead and embed the whole dataset, with all augmented versions too.
-        emb_storage = np.zeros((len(crops) * len(modifiers), 128), np.float32)
-        for start_idx in count(step=10):
-            try:
-                emb = sess.run(endpoints['emb'])
-                print('\rEmbedded batch {}-{}/{}'.format(start_idx, start_idx + len(emb), len(emb_storage)),
-                      flush=True, end='')
-                emb_storage[start_idx:start_idx + len(emb)] = emb
-            except tf.errors.OutOfRangeError:
-                break  # This just indicates the end of the dataset.
+            if len(modifiers) > 1:
+                # Pull out the augmentations into a separate first dimension.
+                emb_storage = emb_storage.reshape(len(crops), len(modifiers), -1)
+                emb_storage = emb_storage.transpose((1, 0, 2))  # (Aug,FID,128D)
 
-        print("Done with embedding, aggregating augmentations...", flush=True)
+                # Aggregate according to the specified parameter.
+                emb_storage = AGGREGATORS[self.aggregator](emb_storage)
+            # print(emb_storage)
+            print(emb_storage.shape)
 
-        if len(modifiers) > 1:
-            # Pull out the augmentations into a separate first dimension.
-            emb_storage = emb_storage.reshape(len(crops), len(modifiers), -1)
-            emb_storage = emb_storage.transpose((1, 0, 2))  # (Aug,FID,128D)
-
-            # Aggregate according to the specified parameter.
-            emb_storage = AGGREGATORS["mean"](emb_storage)
-        # print(emb_storage)
-        print(emb_storage.shape)
-
-        for i in range(1, len(emb_storage)):
-            print("Similarity: {}".format(compare(emb_storage[i], emb_storage[i - 1])))
+            return emb_storage
 
 
 if __name__ == '__main__':
     import os
 
     imgs = []
-    dir = "/home/imesha/Documents/Projects/FYP/Tests/images/recognition/gallery"
+    dir = "/home/imesha/Documents/Projects/FYP/Tests/re-id-dataset/output"
     for file in os.listdir(dir):
         img = cv2.imread(dir + "/" + file)
         imgs.append(img)
-        imgs.append(img)
 
-    # cv2.imshow("Image 1", imgs[0])
-    # cv2.imshow("Image 2", imgs[1])
-    #
-    # cv2.waitKey(0)
-    # sleep(10)
+    imgs = imgs[:200]
 
-    main(imgs, crop_augment="avgpool", flip=False)
+    re_id = TriNetReID(batch_size=32)
+    print("Embedding {} images".format(len(imgs)))
+    embeddings = re_id.embed(imgs)
+
+    print("Comparing embeddings")
+    for i in range(len(imgs)):
+        img = imgs[i]
+        similar = []
+        for j in range(len(imgs)):
+            similarity = compare(embeddings[j], embeddings[i])
+            if similarity < 10:
+                similar.append(imgs[j])
+
+        print("Found {} similar images".format(len(similar)))
+        frame = cv2.resize(img, (25, 50))
+        for s in similar:
+            frame = np.hstack((frame, cv2.resize(s, (25, 50))))
+        cv2.imshow("Similar", frame)
+        cv2.waitKey(5000)
+        cv2.destroyAllWindows()
